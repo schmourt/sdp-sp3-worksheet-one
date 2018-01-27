4d4a47d81c21bd21f023294fb727512fb2b98fa5
Courtney <32526188+schmourt@users.noreply.github.com>
2018-01-27 16:07:06 +0000

    Update Deque.kt

--- exercise-eight/src/Deque.kt
+++ exercise-eight/src/Deque.kt
@@ -2,21 +2,36 @@ class Deque<T> {
     private var head: Element<T>? = null
 
     fun push(value: T) {
-        // TODO
+        var node : Element<T>? = Element(value)
+        if (head != null){
+            head = node
+        }
+        else{
+            head = Element(value, null,null)
+        }
     }
 
     fun pop(): T? {
-        // TODO
-        return shift()
+        var value = head?.value
+        var getValue : Element<T>? = head
+        while (getValue?.next != null){
+            getValue = getValue.next
+        }
+        value = getValue?.value
+        getValue?.prev?.next = null
+        return value
     }
 
     fun unshift(value: T) {
-        // TODO
+        val oldHead = head
+        val newHead = Element(value, null, oldHead)
+        oldHead?.prev = newHead
+        head = newHead
     }
 
     fun shift(): T? {
         val value = head?.value
-        // TODO
+        head = head?.next
         return value
     }
 
