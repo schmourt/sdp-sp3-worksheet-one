434915e49749d10fedf76c19872ed88e9ce44682
Courtney <32526188+schmourt@users.noreply.github.com>
2018-01-27 16:05:25 +0000

    Update PrimeFactorCalculator.kt

--- exercise-six/src/PrimeFactorCalculator.kt
+++ exercise-six/src/PrimeFactorCalculator.kt
@@ -1,12 +1,34 @@
 object PrimeFactorCalculator {
 
     fun primeFactors(int: Int): List<Int> {
-        // TODO
-        return listOf<Int>()
+        val result = mutableListOf<Int>()
+
+        for (i in (2..int)) {
+            var t = true
+            s@ for (j in 2..i - 1) {
+                if (i % j == 0) {
+                    t = false
+                    break@s
+                }
+            }
+            if (t) result.add(i)
+        }
+        return result
     }
 
     fun primeFactors(long: Long): List<Long> {
-        // TODO
-        return listOf<Long>()
+        val result = mutableListOf<Long>()
+
+        for (i in (2L..long)) {
+            var t = true
+            s@ for (j in 2L until long) {
+                if (long % j == 0L) {
+                    t = false
+                    break@s
+                }
+            }
+            if (t) result.add(i)
+        }
+        return result
     }
 }
