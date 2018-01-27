1a0829af6a817fb65aa737bf72cccc91e1cb2661
Courtney <32526188+schmourt@users.noreply.github.com>
2018-01-27 16:06:15 +0000

    Update PascalsTriangle.kt

--- exercise-seven/src/PascalsTriangle.kt
+++ exercise-seven/src/PascalsTriangle.kt
@@ -1,7 +1,18 @@
 object PascalsTriangle {
+    private fun factorial(x : Int): Int{
+        return if (x < 0) - 1 else (if (x == 1 || x == 0) 1 else x * factorial(x-1))
+    }
 
     fun computeTriangle(rows: Int): List<List<Int>> {
-        // TODO
-        return listOf(listOf<Int>())
+        val outside = mutableListOf<List<Int>>()
+        for (i in 1..rows) {
+            for (j in 1..i) {
+                val value = factorial(i) / (factorial(j) * factorial(i - j))
+
+            }
+            return outside
+        }
+
     }
 }
+
